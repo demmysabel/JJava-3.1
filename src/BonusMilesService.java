1dd5d2ead690cf34369c6490445380be4979d035
demmysabel <ekprokhorenko@gmail.com>
2022-04-27 12:55:16 +0300

    BonusMilesService added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int priceForMiles = 20;
+        int cost = price/priceForMiles;
+
+        return cost;
+    }
+
+
+}
